561377dbb47cad4ed5a8255ad844b7986d9b890a
Shreyas77 <shreyasbanagar7@gmail.com>
2018-06-17 19:21:30 +0530

    Completed Week 2 assignment

--- Assignment 2 (Week 3)/machine-learning-ex2 solved/ex2/costFunction.m
+++ Assignment 2 (Week 3)/machine-learning-ex2 solved/ex2/costFunction.m
@@ -19,8 +19,9 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
-
+hypo = sigmoid(X*theta);
+J=-1/m* sum(y.*log(hypo)+(1-y).*log(1-hypo));
+grad = 1/m * X'*(hypo-y);
 
 
 
--- Assignment 2 (Week 3)/machine-learning-ex2 solved/ex2/predict.m
+++ Assignment 2 (Week 3)/machine-learning-ex2 solved/ex2/predict.m
@@ -14,11 +14,14 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
+hypo = sigmoid(X*theta);
+for i=1:m
+    if hypo(i)>=0.5
+        p(i)=1;
+    else
+        p(i)=0;
+    end
+end
 
 
 % =========================================================================
