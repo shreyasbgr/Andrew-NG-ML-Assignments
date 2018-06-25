c20aae12c969e64c405d4fb7c7e999a81c4a0dc9
Shreyas77 <shreyasbanagar7@gmail.com>
2018-06-25 14:02:37 +0530

    Completed one-vs-all classification

--- Assignment 3 (Week 4)/machine-learning-ex3 solved/ex3/predictOneVsAll.m
+++ Assignment 3 (Week 4)/machine-learning-ex3 solved/ex3/predictOneVsAll.m
@@ -29,8 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
+htheta=X*all_theta';
+[temp,p]=max(htheta,[],2);
 
 
 
