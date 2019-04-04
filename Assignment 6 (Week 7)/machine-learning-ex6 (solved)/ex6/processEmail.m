5b31075e2449e1469b492936160d44d76d2f5c52
shreyasbgr <shreyasbanagar7@gmail.com>
2019-04-04 16:38:31 +0530

    Completed Week 7 stuff

--- Assignment 6 (Week 7)/machine-learning-ex6 (solved)/ex6/processEmail.m
+++ Assignment 6 (Week 7)/machine-learning-ex6 (solved)/ex6/processEmail.m
@@ -96,7 +96,11 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
+    for c = 1:length(vocabList)
+      if(strcmp(str, vocabList{c}) == 1)
+        word_indices = [word_indices ; c];
+      end
+    end
 
 
 
